631ba39a8d9c3739b3ccd3da217edf70a7254836
Paresh Panat <84109286+pareshpanat@users.noreply.github.com>
2026-01-23 21:48:06 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@
 
 import { Env, ChatMessage, VectorChunk } from "./types";
 
-const SCOUT_MODEL = "@cf/meta/llama-4-scout-17b-16e-instruct";
+//const SCOUT_MODEL = "@cf/meta/llama-4-scout-17b-16e-instruct";
+const SCOUT_MODEL = "@cf/openai/gpt-oss-120b";
 const COMPLEX_MODEL = "@cf/openai/gpt-oss-120b";
+//const COMPLEX_MODEL = "@cf/meta/llama-3.3-70b-instruct-fp8-fast";
 
 /* -------------------- CORS HEADERS -------------------- */
 
